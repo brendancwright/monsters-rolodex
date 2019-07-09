1a9b5493175d7d512edb7eea61c4e393c98bb4d7
unknown <brendancwright@gmail.com>
2019-07-09 12:44:22 -0400

    Updates

---
+++ precache-manifest.92a6286367fdff0e9721443510a74785.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eff6604686f64688978baedfb053413",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "928bd739c7d710977385",
+    "url": "/monsters-rolodex/static/css/main.aa7de1bf.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "928bd739c7d710977385",
+    "url": "/monsters-rolodex/static/js/main.9fe5a411.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
